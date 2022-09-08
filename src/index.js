9d1c509ca201e49e5f40ed25ec1854f8a0de4725
ambmuna <arnauldmuna@gmail.com>
2022-09-08 12:13:39 +0200

    second commit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
